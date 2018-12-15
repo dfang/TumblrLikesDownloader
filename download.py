efc4398801a6eb6f5398f6ef5044a66c83135692
fang duan <df1228@gmail.com>
2018-12-15 23:07:33 +0800

    fix urllib.urlretrieve error

--- download.py
+++ download.py
@@ -3,9 +3,17 @@
 
 import pytumblr
 import os
+import sys
 import urllib
 import re
 from tumblr_keys import *
+if sys.version_info[0] >= 3:
+    from urllib.request import urlretrieve
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib import urlretrieve
 
 client = pytumblr.TumblrRestClient(
     consumer_key,
@@ -20,12 +28,12 @@ def media_download(mylikes, dirname):
             for j in range(0, len(mylikes['liked_posts'][i]['photos'])):
                 url = mylikes['liked_posts'][i]['photos'][j]['original_size']['url']
                 name = re.findall(r'tumblr_\w+.\w+', url)
-                urllib.urlretrieve(url, dirname + '/' + name[0])
+                urlretrieve(url, dirname + '/' + name[0])
             print('[' + str(i+1) + ':' + str(len(mylikes['liked_posts'])) + ']')
         elif mylikes['liked_posts'][i]['type'] == 'video':
             url = mylikes['liked_posts'][i]['video_url']
             name = re.findall(r'tumblr_\w+.\w+', url)
-            urllib.urlretrieve(url, dirname + '/' + name[0])
+            urlretrieve(url, dirname + '/' + name[0])
             print('[' + str(i+1) + ':' + str(len(mylikes['liked_posts'])) + ']')
         else:
             print('[' + str(i+1) + ':' + str(len(mylikes['liked_posts'])) + ']')
